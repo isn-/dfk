5a30a9596f0dfc4937a90c3f2fba80b337513102
Stanislav Ivochkin <isn@extrn.org>
2016-01-18 21:02:47 +0300

    buf: Replace asserts with NULL checks

--- src/buf.c
+++ src/buf.c
@@ -1,13 +1,15 @@
-#include <assert.h>
 #include <dfk/core.h>
 #include <dfk/buf.h>
+#include "common.h"
 
 int dfk_buf_init(dfk_buf_t* buf, dfk_context_t* ctx, size_t nbytes)
 {
-  assert(buf);
+  if (buf == NULL) {
+    return dfk_err_badarg;
+  }
   buf->_.context = ctx;
   buf->size = nbytes;
-  buf->data = ctx->malloc(ctx->userdata, nbytes);
+  buf->data = DFK_MALLOC(ctx, nbytes);
   if (buf->data == NULL) {
     return dfk_err_out_of_memory;
   }
@@ -16,7 +18,10 @@ int dfk_buf_init(dfk_buf_t* buf, dfk_context_t* ctx, size_t nbytes)
 
 int dfk_buf_free(dfk_buf_t* buf)
 {
-  assert(buf);
-  buf->_.context->free(buf->_.context->userdata, buf->data);
+  if (buf == NULL) {
+    return dfk_err_badarg;
+  }
+  DFK_FREE(buf->_.context, buf->data);
   return dfk_err_ok;
 }
+
