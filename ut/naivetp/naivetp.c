2cd3442070f7d543a83a6f3f25d9253e69c8c00f
Stanislav Ivochkin <isn@extrn.org>
2016-02-23 21:38:23 +0300

    ut: Add graceful shutdown to naivetp server

--- ut/naivetp/naivetp.c
+++ ut/naivetp/naivetp.c
@@ -28,6 +28,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <errno.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -40,6 +41,7 @@ typedef struct _naivetp_server_t {
   int sock;
   pthread_t thread;
   dfk_context_t* ctx;
+  struct client_t* clients;
 } _naivetp_server_t;
 
 
@@ -47,25 +49,30 @@ typedef struct client_t {
   int sock;
   pthread_t thread;
   dfk_context_t* ctx;
+  struct client_t* next;
+  _naivetp_server_t* srv;
 } client_t;
 
 
 static void* conn_thread(void* arg)
 {
   client_t* c = (client_t*) arg;
-  DFK_INFO(c->ctx, "spawned");
+  DFK_INFO(c->ctx, "{%p} spawned", (void*) c);
   while (1) {
     ssize_t nread = 0;
     char buf[512];
     if ((nread = read(c->sock, buf, sizeof(buf))) > 0) {
-      ssize_t nwritten = write(c->sock, buf, nread);
-      (void) nwritten;
+      ssize_t nwritten = 0;
+      DFK_INFO(c->ctx, "{%p} bytes read %ld", (void*) c, (long) nread);
+      nwritten = write(c->sock, buf, nread);
+      DFK_INFO(c->ctx, "{%p} bytes written %ld", (void*) c, (long) nwritten);
     } else {
       break;
     }
   }
-  free(c);
-  return NULL;
+  DFK_INFO(c->ctx, "{%p} close", (void*) c);
+  (void) close(c->sock);
+  return c;
 }
 
 
@@ -76,17 +83,24 @@ static void* main_thread(void* arg)
   socklen_t addrlen = sizeof(addr);
   int csock;
 
-  DFK_INFO(s->ctx, "spawned");
+  DFK_INFO(s->ctx, "{%p} spawned", (void*) s);
 
   while ((csock = accept(s->sock, (struct sockaddr*) &addr, &addrlen)) > 0) {
     client_t* newclient = NULL;
-    DFK_INFO(s->ctx, "new connection %s:%d", inet_ntoa(addr.sin_addr), addr.sin_port);
+    DFK_INFO(s->ctx, "{%p} new connection %s:%d", (void*) s, inet_ntoa(addr.sin_addr), addr.sin_port);
     newclient = (client_t*) calloc(1, sizeof(client_t));
     newclient->sock = csock;
+    newclient->ctx = s->ctx;
+    newclient->next = s->clients;
+    newclient->srv = s;
+    s->clients = newclient;
     if (pthread_create(&newclient->thread, NULL, conn_thread, newclient) != 0) {
+      DFK_ERROR(s->ctx, "can not create thread");
       break;
     }
   }
+  DFK_INFO(s->ctx, "{%p} close", (void*) s);
+  (void) close(s->sock);
   return NULL;
 }
 
@@ -95,8 +109,13 @@ naivetp_server_t* naivetp_server_start(dfk_context_t* ctx, uint16_t port)
 {
   naivetp_server_t* s = NULL;
   struct sockaddr_in addr;
+  int reuse = 1;
 
-  s = (void*) calloc(1, sizeof(_naivetp_server_t));
+  if (ctx == NULL) {
+    return NULL;
+  }
+
+  s = (void*) ctx->malloc(NULL, sizeof(_naivetp_server_t));
   if (s == NULL) {
     return NULL;
   }
@@ -106,17 +125,25 @@ naivetp_server_t* naivetp_server_start(dfk_context_t* ctx, uint16_t port)
   if (s->sock < 0) {
     goto cleanup;
   }
+  s->clients = NULL;
 
   addr.sin_port = htons(port);
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
   if (bind(s->sock, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+    DFK_ERROR(ctx, "can not bind socket: %s", strerror(errno));
+    goto cleanup;
+  }
+
+  if (setsockopt(s->sock, SOL_SOCKET, SO_REUSEADDR, (const char*) &reuse, sizeof(reuse)) < 0) {
+    DFK_ERROR(ctx, "enable SO_REUSEADDR failed: %s", strerror(errno));
     goto cleanup;
   }
 
   DFK_INFO(ctx, "listen to port %d", port);
   if (listen(s->sock, -1) < 0) {
+    DFK_ERROR(ctx, "can not listen: %s", strerror(errno));
     goto cleanup;
   }
 
@@ -129,7 +156,7 @@ naivetp_server_t* naivetp_server_start(dfk_context_t* ctx, uint16_t port)
 cleanup:
   DFK_ERROR(ctx, "failed");
   if (s) {
-    free(s);
+    ctx->free(NULL, s);
     s = NULL;
   }
   return s;
@@ -138,8 +165,21 @@ cleanup:
 void naivetp_server_stop(naivetp_server_t* srv)
 {
   void* retval;
-  (void) close(srv->sock);
+  client_t* client;
+
+  if (srv == NULL) {
+    return;
+  }
+
+  client = srv->clients;
+  (void) shutdown(srv->sock, SHUT_RDWR);
   pthread_join(srv->thread, &retval);
-  free(srv);
+  while (client) {
+    (void) shutdown(client->sock, SHUT_RDWR);
+    pthread_join(client->thread, &retval);
+    client = client->next;
+    srv->ctx->free(NULL, retval);
+  }
+  srv->ctx->free(NULL, srv);
 }
 
