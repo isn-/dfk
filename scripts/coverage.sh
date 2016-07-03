7ea7b55c76e4ecc050174b539674026ab1938a73
Stanislav Ivochkin <isn@extrn.org>
2016-07-03 14:13:18 +0300

    devenv: Generate coverage report localy

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,9 +2,20 @@
 
 lcov --directory . --capture --output-file lcov.info
 lcov --remove lcov.info '*/thirdparty/*' --output lcov.info
+lcov --remove lcov.info '*cpm_packages*' --output lcov.info
+lcov --remove lcov.info '*valgrind*' --output lcov.info
 lcov --remove lcov.info '*/ut/*' --output lcov.info
 
-# deploy
 if [ "$TRAVIS" == "true" ]; then
+  # deploy
   bash <(curl -s https://codecov.io/bash)
+else
+  # generate html localy
+  mkdir -p doc/coverage
+  mv lcov.info doc/coverage/
+  pushd doc/coverage
+  genhtml lcov.info
+  echo
+  echo Open $(pwd)/index.html in your browser for the coverage report
+  popd >/dev/null
 fi
