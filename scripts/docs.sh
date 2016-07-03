37cb2721b7807c556c1b8b08a2107a57853891b1
Stanislav Ivochkin <isn@extrn.org>
2016-07-03 13:48:51 +0300

    devenv: Re-generate OAuth token

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -23,7 +23,7 @@ if [ "$TRAVIS" == "true" ]; then
   git init
   git add .
   git commit -m "Deploy to Github Pages"
-  git push --force --quiet "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" master:gh-pages
+  git push --force --quiet "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" master:gh-pages  > /dev/null 2>&1
   popd
   exit 0
 fi
