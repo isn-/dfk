d459d3eac06f507bdec173f2bdcd182e8bb0c054
Stanislav Ivochkin <isn@extrn.org>
2016-09-20 20:36:33 +0300

    http: Don't parse empty query

--- src/http/request.c
+++ src/http/request.c
@@ -405,34 +405,36 @@ int dfk_http_request_read_headers(dfk_http_request_t* req)
   req->port = urlparser.port;
 
   /* Parse query */
-  char* decoded_query = dfk_arena_alloc(req->_request_arena, req->query.size);
-  size_t decoded_query_size;
-  size_t bytesdecoded = dfk_urldecode(req->query.data, req->query.size,
-      decoded_query, &decoded_query_size);
-  if (bytesdecoded != req->query.size) {
-    return dfk_err_protocol;
-  }
+  if (req->query.size) {
+    char* decoded_query = dfk_arena_alloc(req->_request_arena, req->query.size);
+    size_t decoded_query_size;
+    size_t bytesdecoded = dfk_urldecode(req->query.data, req->query.size,
+        decoded_query, &decoded_query_size);
+    if (bytesdecoded != req->query.size) {
+      return dfk_err_protocol;
+    }
 
-  char* decoded_query_end = decoded_query + decoded_query_size;
-  char* key_begin = decoded_query;
-  char* key_end = NULL;
-  char* value_begin = NULL;
+    char* decoded_query_end = decoded_query + decoded_query_size;
+    char* key_begin = decoded_query;
+    char* key_end = NULL;
+    char* value_begin = NULL;
 
-  for (char* i = decoded_query; i != decoded_query_end; ++i) {
-    if (*i == '=') {
-      key_end = i - 1;
-      value_begin = i + 1;
-    } else if (*i == '&') {
-      key_begin = i + 1;
-      dfk_strmap_item_t* item = dfk_arena_alloc(req->_request_arena, sizeof(dfk_strmap_item_t));
-      if (!item) {
-        return dfk_err_nomem;
+    for (char* i = decoded_query; i != decoded_query_end; ++i) {
+      if (*i == '=') {
+        key_end = i - 1;
+        value_begin = i + 1;
+      } else if (*i == '&') {
+        key_begin = i + 1;
+        dfk_strmap_item_t* item = dfk_arena_alloc(req->_request_arena, sizeof(dfk_strmap_item_t));
+        if (!item) {
+          return dfk_err_nomem;
+        }
+        assert(key_begin <= key_end);
+        assert(value_begin <= i);
+        item->key = (dfk_buf_t) {(char*) key_begin, key_end - key_begin};
+        item->value = (dfk_buf_t) {(char*) value_begin, i - 1 - value_begin};
+        dfk_strmap_insert(&req->arguments, item);
       }
-      assert(key_begin <= key_end);
-      assert(value_begin <= i);
-      item->key = (dfk_buf_t) {(char*) key_begin, key_end - key_begin};
-      item->value = (dfk_buf_t) {(char*) value_begin, i - 1 - value_begin};
-      dfk_strmap_insert(&req->arguments, item);
     }
   }
   return dfk_err_ok;
