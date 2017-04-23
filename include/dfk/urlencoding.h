ea4bd25bffbf7604a8628eb2bba0ea4fad1c280b
Stanislav Ivochkin <isn@extrn.org>
2017-04-23 23:01:15 +0300

    core: Restore url-encoding routines

--- include/dfk/urlencoding.h
+++ include/dfk/urlencoding.h
@@ -2,6 +2,8 @@
  * @file dfk/urlencoding.h
  * URL decoding and encoding functions
  *
+ * @see RFC-3986 https://tools.ietf.org/html/rfc3986
+ *
  * @copyright
  * Copyright (c) 2016-2017 Stanislav Ivochkin
  * Licensed under the MIT License (see LICENSE)
@@ -11,13 +13,26 @@
 #include <stddef.h>
 
 /**
- * Decode url-encoded buffer buf of size size.
+ * Returns size of the buffer needed to store the result of percent-decoding.
+ */
+size_t dfk_urldecode_hint(const char* buf, size_t size);
+
+/**
+ * Decode percent-encoded buffer.
  *
  * If out is NULL in-place decoding is performed.
  *
  * @return Number of bytes consumed
  */
-size_t dfk_urldecode(char* buf, size_t size, char* out, size_t* byteswritten);
+size_t dfk_urldecode(char* buf, size_t size, char* out, size_t* nwritten);
 
+/**
+ * Returns size of the buffer needed to store the result of percent-encoding.
+ */
+size_t dfk_urlencode_hint(const char* buf, size_t size);
+
+/**
+ * Percent encode buffer.
+ */
 size_t dfk_urlencode(const char* buf, size_t size, char* out);
 
--- src/urlencoding.c
+++ src/urlencoding.c
@@ -47,11 +47,97 @@ static const uint8_t hex_to_byte[] = {
 };
 
 
-size_t dfk_urldecode(char* buf, size_t size, char* out, size_t* byteswritten)
+static const uint8_t bytelo_to_hex[] = {
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  '0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+};
+
+
+static const uint8_t bytehi_to_hex[] = {
+  '0', '0', '0', '0', '0', '0', '0', '0',
+  '0', '0', '0', '0', '0', '0', '0', '0',
+  '1', '1', '1', '1', '1', '1', '1', '1',
+  '1', '1', '1', '1', '1', '1', '1', '1',
+  '2', '2', '2', '2', '2', '2', '2', '2',
+  '2', '2', '2', '2', '2', '2', '2', '2',
+  '3', '3', '3', '3', '3', '3', '3', '3',
+  '3', '3', '3', '3', '3', '3', '3', '3',
+  '4', '4', '4', '4', '4', '4', '4', '4',
+  '4', '4', '4', '4', '4', '4', '4', '4',
+  '5', '5', '5', '5', '5', '5', '5', '5',
+  '5', '5', '5', '5', '5', '5', '5', '5',
+  '6', '6', '6', '6', '6', '6', '6', '6',
+  '6', '6', '6', '6', '6', '6', '6', '6',
+  '7', '7', '7', '7', '7', '7', '7', '7',
+  '7', '7', '7', '7', '7', '7', '7', '7',
+  '8', '8', '8', '8', '8', '8', '8', '8',
+  '8', '8', '8', '8', '8', '8', '8', '8',
+  '9', '9', '9', '9', '9', '9', '9', '9',
+  '9', '9', '9', '9', '9', '9', '9', '9',
+  'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+  'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+  'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B',
+  'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B',
+  'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C',
+  'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C',
+  'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D',
+  'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D',
+  'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+  'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+  'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F',
+  'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F',
+};
+
+
+size_t dfk_urldecode_hint(const char* buf, size_t size)
+{
+  size_t s = 0;
+  for (const char* end = buf + size; buf != end; ++buf) {
+    if (*buf == '%') {
+      s += 2;
+    }
+  }
+  return size - s;
+}
+
+
+size_t dfk_urldecode(char* buf, size_t size, char* out, size_t* nwritten)
 {
   assert(buf || !size);
   assert(out || !size);
-  assert(byteswritten);
+  assert(nwritten);
 
   char* end = buf + size;
   char* initial_out = out;
@@ -71,17 +157,45 @@ size_t dfk_urldecode(char* buf, size_t size, char* out, size_t* byteswritten)
       *out++ = *buf++;
     }
   }
-  *byteswritten = out - initial_out;
+  *nwritten = out - initial_out;
   return buf - end + size;
+}
+
+
+static int dfk__urlencode_unreserved(char c)
+{
+  return ('a' <= c && c <= 'z')
+    || ('A' <= c && c <= 'Z')
+    || ('0' <= c && c <= '9')
+    || c == '-' || c == '.' || c == '_' || c == '~';
+}
+
+
+size_t dfk_urlencode_hint(const char* buf, size_t size)
+{
+  size_t s = 0;
+  for (const char* end = buf + size; buf != end; ++buf) {
+    if (!dfk__urlencode_unreserved(*buf)) {
+      s += 2;
+    }
+  }
+  return size + s;
 }
 
 
 size_t dfk_urlencode(const char* buf, size_t size, char* out)
 {
-  /** @todo implement it */
-  (void) (buf);
-  (void) (size);
-  (void) (out);
-  return 0;
+  char* original_out = out;
+  for (const char* end = buf + size; buf != end; ++buf) {
+    char b = *buf;
+    if (dfk__urlencode_unreserved(b)) {
+      *out++ = b;
+    } else {
+      *out++ = '%';
+      *out++ = bytehi_to_hex[(unsigned char) b];
+      *out++ = bytelo_to_hex[(unsigned char) b];
+    }
+  }
+  return out - original_out;
 }
 
