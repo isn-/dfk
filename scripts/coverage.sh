5481d55afd15eaaae7527dcd587f5e3355179851
Stanislav Ivochkin <isn@extrn.org>
2017-04-28 18:26:21 +0300

    devenv: Fix coverage.sh on Travis

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -9,11 +9,20 @@
 # * default (no special environment variables set) - create local html report
 # * CI ("$TRAVIS" == "true") - upload coverage report to codecov.io
 
-lcov --rc lcov_branch_coverage=1 --directory . --capture --output-file lcov.info
-lcov --rc lcov_branch_coverage=1 --remove lcov.info '*/thirdparty/*' --output lcov.info
-lcov --rc lcov_branch_coverage=1 --remove lcov.info '*cpm_packages*' --output lcov.info
-lcov --rc lcov_branch_coverage=1 --remove lcov.info '*valgrind*' --output lcov.info
-lcov --rc lcov_branch_coverage=1 --remove lcov.info '*/ut/*' --output lcov.info
+if [ "$TRAVIS" == "true" ]; then
+  # lcov on Travis (ubuntu 12.04) does not recognize --rc option
+  lcov_args=""
+  genhtml_args=""
+else
+  lcov_args="--rc lcov_branch_coverage=1"
+  genhtml_args="--rc genhtml_branch_coverage=1"
+fi
+
+lcov $lcov_args --directory . --capture --output-file lcov.info
+lcov $lcov_args --remove lcov.info '*/thirdparty/*' --output lcov.info
+lcov $lcov_args --remove lcov.info '*cpm_packages*' --output lcov.info
+lcov $lcov_args --remove lcov.info '*valgrind*' --output lcov.info
+lcov $lcov_args --remove lcov.info '*/ut/*' --output lcov.info
 
 if [ "$TRAVIS" == "true" ]; then
   # deploy
@@ -23,7 +32,7 @@ else
   mkdir -p doc/coverage
   mv lcov.info doc/coverage/
   pushd doc/coverage
-  genhtml --rc genhtml_branch_coverage=1 --function-coverage --branch-coverage lcov.info
+  genhtml $genhtml_args --function-coverage --branch-coverage lcov.info
   echo
   echo Open file://$(pwd)/index.html in your browser for the coverage report
   popd >/dev/null
