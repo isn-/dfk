aefbe399b850a90addfa35598264730d789e53fb
Stanislav Ivochkin <isn@extrn.org>
2017-05-18 23:05:40 +0300

    net: dfk_tcp_socket_t - work in progress

--- include/dfk/tcp_socket.h
+++ include/dfk/tcp_socket.h
@@ -1,4 +1,7 @@
 /**
+ * @file dfk/tcp_socket.h
+ * Contains a definitions of dfk_tcp_socket_t and related routines.
+ *
  * @copyright
  * Copyright (c) 2015-2017 Stanislav Ivochkin
  * Licensed under the MIT License (see LICENSE)
@@ -6,57 +9,57 @@
 
 #pragma once
 #include <stdint.h>
-#include <sys/types.h>
-#include <dfk/core.h>
-
-
-/**
- * @file dfk/tcp_socket.h
- * TCP socket object and related functions
- *
- * @addtogroup net net
- * @{
- */
+#include <unistd.h>
+#include <dfk/context.h>
+#include <dfk/misc.h>
 
 /**
  * TCP socket object
  */
 typedef struct dfk_tcp_socket_t {
   /**
-   * @privatesection
-   */
-
-  int _socket;
-  int32_t _flags;
-
-  /**
    * @publicsection
    */
-
   dfk_t* dfk;
   dfk_userdata_t user;
+
+  /** @private */
+  int _socket;
 } dfk_tcp_socket_t;
 
 
 /**
- * Initialize tcp_socket object
+ * Initialize TCP socket object
+ *
+ * @return
+ * - ::dfk_err_ok On success
+ * - ::dfk_err_sys If socket(2) syscall failed
  */
 int dfk_tcp_socket_init(dfk_tcp_socket_t* sock, dfk_t* dfk);
 
-
 /**
- * Cleaup resources allocated for tcp_socket object
+ * Close socket.
+ *
+ * Any operation currently running will be interrupted. Resources allocated to
+ * the socket will be released.
  */
-int dfk_tcp_socket_free(dfk_tcp_socket_t* sock);
-
+int dfk_tcp_socket_close(dfk_tcp_socket_t* sock);
 
 /**
  * Connect to TCP endpoint
  */
-int dfk_tcp_socket_connect(
-    dfk_tcp_socket_t* sock,
-    const char* endpoint,
-    uint16_t port);
+int dfk_tcp_socket_connect(dfk_tcp_socket_t* sock,
+    const char* endpoint, uint16_t port);
+
+/**
+ * Read at most nbytes from the socket
+ */
+ssize_t dfk_tcp_socket_read(dfk_tcp_socket_t* sock, char* buf, size_t nbytes);
+
+/**
+ * Write data to socket
+ */
+ssize_t dfk_tcp_socket_write(dfk_tcp_socket_t* sock, char* buf, size_t nbytes);
 
 
 /**
@@ -64,7 +67,6 @@ int dfk_tcp_socket_connect(
  *
  * To stop listening, call dfk_tcp_socket_close.
  * Callback is executed for each incoming connection.
- */
 int dfk_tcp_socket_listen(
     dfk_tcp_socket_t* sock,
     const char* endpoint,
@@ -72,60 +74,35 @@ int dfk_tcp_socket_listen(
     void (*callback)(dfk_coro_t*, dfk_tcp_socket_t*, void*),
     void* cbarg,
     size_t backlog);
-
-
-/**
- * Close socket. Any operation currently running will be interrupted
  */
-int dfk_tcp_socket_close(dfk_tcp_socket_t* sock);
 
 
 /**
  * Close the outgoing part of the socket.
- */
 int dfk_tcp_socket_shutdown(dfk_tcp_socket_t* sock);
+ */
 
 
 /**
  * Wait for other side of the socket to close connection.
- */
 int dfk_tcp_socket_wait_disconnect(dfk_tcp_socket_t* sock, uint64_t timeout);
-
-
-/**
- * Read at most nbytes from the socket
  */
-ssize_t dfk_tcp_socket_read(
-    dfk_tcp_socket_t* sock,
-    char* buf,
-    size_t nbytes);
 
 
 /**
  * Read data from the socket into several buffers at once
- */
 ssize_t dfk_tcp_socket_readv(
     dfk_tcp_socket_t* sock,
     dfk_iovec_t* iov,
     size_t niov);
-
-
-/**
- * Write data to socket
  */
-ssize_t dfk_tcp_socket_write(
-    dfk_tcp_socket_t* sock,
-    char* buf,
-    size_t nbytes);
 
 
 /**
  * Write data from several buffers to socket at once
- */
 ssize_t dfk_tcp_socket_writev(
     dfk_tcp_socket_t* sock,
     dfk_iovec_t* iov,
     size_t niov);
-
-/** @} */
+ */
 
--- src/tcp_socket.c
+++ src/tcp_socket.c
@@ -4,171 +4,111 @@
  * Licensed under the MIT License (see LICENSE)
  */
 
+#define _BSD_SOURCE
 #include <assert.h>
-#include <errno.h>
-#include <sys/epoll.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <dfk.h>
+#include <dfk/tcp_socket.h>
+#include <dfk/error.h>
 #include <dfk/internal.h>
-
-/* TCP Socket states
- * (*)        -> spare
- * spare      -> listening
- *            -> connecting
- * listening  -> spare
- *            -> closing
- * connecting -> connected
- *            -> spare (connection failed)
- * connected  -> reading
- *            -> writing
- *            -> closing
- * reading    -> connected
- *            -> closing
- * writing    -> connected
- *            -> closing
- * closing    -> closed
- * closed     ->
- */
-#define TCP_SOCKET_SPARE ((int) 1)
-#define TCP_SOCKET_LISTENING ((int) 1 << 1)
-#define TCP_SOCKET_CONNECTING ((int) 1 << 2)
-#define TCP_SOCKET_CONNECTED ((int) 1 << 3)
-#define TCP_SOCKET_READING ((int) 1 << 4)
-#define TCP_SOCKET_WRITING ((int) 1 << 5)
-#define TCP_SOCKET_CLOSING ((int) 1 << 6)
-#define TCP_SOCKET_CLOSED ((int) 1 << 7)
-
-#define STATE(sock) ((sock)->_flags & 0xFF)
-#define TO_STATE(sock, state) ((sock)->_flags = ((sock)->_flags & 0xFFFFFF00) | (state))
-
-/* Accepted socket has an accociated coroutine and hence
- * requires explicit coroutine join upon closing
- */
-#define TCP_SOCKET_IS_ACCEPTED ((int) 1 << 8)
-#define TCP_SOCKET_IS_LISTENING ((int) 1 << 9)
-#define TCP_SOCKET_DETACHED ((int) 1 << 10)
-
-#define FLAG(sock, flag) (((sock)->_flags & 0xFFFFFF00) & (flag))
-
 
 int dfk_tcp_socket_init(dfk_tcp_socket_t* sock, dfk_t* dfk)
 {
-  if (!sock || !dfk) {
-    return dfk_err_badarg;
-  }
+  assert(sock);
+  assert(dfk);
   DFK_DBG(dfk, "{%p}", (void*) sock);
-
-  sock->_socket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-  if (sock->_socket == -1) {
-    DFK_ERROR(dfk, "socket() call failed with code %d: %s", errno, strerror(errno));
-    dfk->sys_errno = errno;
+  int s = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+  if (s == -1) {
+    DFK_ERROR_SYSCALL(dfk, "socket");
     return dfk_err_sys;
   }
-
-  sock->_arg.func = NULL;
-  sock->_flags = TCP_SOCKET_SPARE;
+  sock->_socket = s;
   sock->dfk = dfk;
   return dfk_err_ok;
 }
 
-
-int dfk_tcp_socket_free(dfk_tcp_socket_t* sock)
+int dfk_tcp_socket_close(dfk_tcp_socket_t* sock)
 {
-  if (!sock) {
-    return dfk_err_badarg;
-  }
-  DFK_DBG(sock->dfk, "{%p}", (void*) sock);
-  if (STATE(sock) != TCP_SOCKET_CLOSED) {
-    DFK_CALL(sock->dfk, dfk_tcp_socket_close(sock));
+  assert(sock);
+  assert(sock->dfk);
+  dfk_t* dfk = sock->dfk;
+  DFK_INFO(dfk, "{%p} closing", (void*) sock);
+  if (close(sock->_socket) == -1) {
+    DFK_ERROR_SYSCALL(dfk, "close");
+    return dfk_err_sys;
   }
   return dfk_err_ok;
 }
 
-
-/**
- * Sets connect result and yields back to dfk_tcp_socket_connect caller
- */
-static void dfk__tcp_socket_on_connect(
-    int status,
-    dfk_epoll_cb_arg_t arg0,
-    dfk_epoll_cb_arg_t arg1,
-    dfk_epoll_cb_arg_t arg2,
-    dfk_epoll_cb_arg_t arg3)
+int dfk_tcp_socket_connect(dfk_tcp_socket_t* sock,
+    const char* endpoint, uint16_t port)
 {
-  dfk_tcp_socket_t* sock = (dfk_tcp_socket_t*) arg0.ptr;
-  int* err = (int*) arg1.ptr;
-  dfk_coro_t* yieldback = (dfk_coro_t*) arg2.ptr;
-  DFK_UNUSED(arg3);
-
   assert(sock);
-  assert(STATE(sock) == TCP_SOCKET_CONNECTING);
+  assert(endpoint);
+  dfk_t* dfk = sock->dfk;
 
-  DFK_DBG(sock->dfk, "{%p} connected with status %d (%s)",
-      (void*) sock, status, status ? "error" : "no error");
-
-  epoll_ctl(sock->dfk->_epollfd, EPOLL_CTL_DEL, sock->_socket, NULL);
-
-  if (status != 0) {
-    sock->dfk->sys_errno = status;
-    *err = dfk_err_sys;
-  } else {
-    *err = dfk_err_ok;
-  }
-  DFK_IO_RESUME(sock->dfk, yieldback);
-}
-
-
-int dfk_tcp_socket_connect(
-    dfk_tcp_socket_t* sock,
-    const char* endpoint,
-    uint16_t port)
-{
-  if (!sock || !endpoint) {
-    return dfk_err_badarg;
-  }
-  if (STATE(sock) == TCP_SOCKET_CONNECTING) {
-    return dfk_err_inprog;
-  }
-  if (STATE(sock) != TCP_SOCKET_SPARE) {
-    return dfk_err_badarg;
-  }
-
-  DFK_DBG(sock->dfk, "{%p} connect to %s:%d", (void*) sock, endpoint, port);
+  DFK_DBG(dfk, "{%p} connect to %s:%d", (void*) sock, endpoint, port);
 
   struct sockaddr_in dest;
   memset(&dest, 0, sizeof(dest));
   dest.sin_family = AF_INET;
   dest.sin_port = htons(port);
   inet_aton(endpoint, (struct in_addr*) &dest.sin_addr.s_addr);
-
-  if (connect(sock->_socket, &dest, sizeof(dest)) == -1) {
-    DFK_ERROR(sock->dfk, "connect() failed with code %d: %s", errno, strerror(errno));
-    sock->dfk->sys_errno = errno;
-    return dfk_err_sys;
+  int ret = connect(sock->_socket, (struct sockaddr *) &dest, sizeof(dest));
+  if (ret == -1) {
+    DFK_DBG(dfk, "{%p} connect returned -1, errno=%d: %s",
+        (void*) sock, errno, strerror(errno));
+    if (errno == EINPROGRESS) {
+      int ioret = DFK_IO(dfk, sock->_socket, DFK_IO_IN | DFK_IO_OUT);
+      DFK_DBG(dfk, "{%p} DFK_IO returned %d", (void*) sock, ioret);
+      if (ioret & DFK_IO_ERR) {
+        DFK_ERROR_SYSCALL(dfk, "connect)");
+        return dfk_err_sys;
+      }
+    }
   }
-
-  int err = dfk_err_ok;
-  dfk_epoll_arg_t arg = {
-    .callback = dfk__tcp_socket_on_connect,
-    .arg0 = {.ptr = sock},
-    .arg1 = {.ptr = &err},
-    .arg2 = {.ptr = DFK_THIS_CORO(sock->dfk)},
-    .arg3 = {.ptr = NULL}
-  };
-
-  struct epoll_event event;
-  event.data.ptr = &arg;
-  event.events = EPOLLOUT;
-  epoll_ctl(sock->dfk->_epollfd, EPOLL_CTL_ADD, sock->_socket, &event);
-
-  TO_STATE(sock, TCP_SOCKET_CONNECTING);
-  DFK_IO(sock->dfk);
-  TO_STATE(sock, err = dfk_err_ok ? TCP_SOCKET_CONNECTED : TCP_SOCKET_SPARE);
-
-  return err;
+  return dfk_err_ok;
 }
 
+ssize_t dfk_tcp_socket_read(dfk_tcp_socket_t* sock, char* buf, size_t nbytes)
+{
+  assert(sock);
+  assert(buf);
+  assert(nbytes);
+  dfk_t* dfk = sock->dfk;
 
+  ssize_t nread = read(sock->_socket, buf, nbytes);
+  DFK_DBG(dfk, "{%p} first read (possibly blocking) attempt returned %lld",
+      (void*) sock, (long long) nread);
+  if (nread >= 0) {
+    return nread;
+  }
+  if (nread != EAGAIN) {
+    DFK_ERROR_SYSCALL(dfk, "read");
+    dfk->dfk_errno = dfk_err_sys;
+    return -1;
+  }
+  int ioret = DFK_IO(dfk, sock->_socket, DFK_IO_IN);
+  DFK_DBG(dfk, "{%p} DFK_IO returned %d", (void*) sock, ioret);
+  if (ioret & DFK_IO_ERR) {
+    DFK_ERROR_SYSCALL(dfk, "read");
+    dfk->dfk_errno = dfk_err_sys;
+    return -1;
+  }
+  assert(ioret & DFK_IO_IN);
+  nread = read(sock->_socket, buf, nbytes);
+  DFK_DBG(dfk, "{%p} read returned %lld", (void*) sock, (long long) nread);
+  if (nread < 0) {
+    DFK_ERROR_SYSCALL(dfk, "read");
+    dfk->dfk_errno = dfk_err_sys;
+    return -1;
+  }
+  return nread;
+}
+
+/*
 typedef struct dfk_tcp_socket_accepted_main_arg_t {
   uv_stream_t* stream;
   void (*callback)(dfk_coro_t*, dfk_tcp_socket_t*, void*);
@@ -195,10 +135,8 @@ static void dfk__tcp_socket_accepted_main(dfk_coro_t* coro, void* p)
 }
 
 
-/**
  * When socket is in LISTENING state, a pointer to
  * dfk_tcp_socket_listen_arg_t is stored in sock->_arg
- */
 typedef struct dfk_tcp_socket_listen_obj_t {
   dfk_coro_t* yieldback;
   dfk_coro_t* close_yieldback;
@@ -291,56 +229,6 @@ int dfk_tcp_socket_listen(
 }
 
 
-static void dfk__tcp_socket_on_close(uv_handle_t* p)
-{
-  dfk_tcp_socket_t* sock;
-
-  assert(p);
-  sock = (dfk_tcp_socket_t*) p->data;
-  assert(sock);
-  assert(sock->dfk);
-  assert(STATE(sock) & TCP_SOCKET_CLOSING);
-  DFK_INFO(sock->dfk, "{%p} is now closed", (void*) sock);
-  if (STATE(sock) & TCP_SOCKET_LISTENING) {
-    dfk_tcp_socket_listen_obj_t* obj = (dfk_tcp_socket_listen_obj_t*) sock->_arg.data;
-    DFK_IO_RESUME(sock->dfk, obj->close_yieldback);
-    DFK_IO_RESUME(sock->dfk, obj->yieldback);
-  } else {
-    DFK_IO_RESUME(sock->dfk, (dfk_coro_t*) sock->_arg.data);
-  }
-}
-
-
-int dfk_tcp_socket_close(dfk_tcp_socket_t* sock)
-{
-  if (!sock) {
-    return dfk_err_badarg;
-  }
-  if (STATE(sock) & TCP_SOCKET_CLOSING) {
-    return dfk_err_inprog;
-  }
-  if (STATE(sock) == TCP_SOCKET_CLOSED) {
-    return dfk_err_ok;
-  }
-
-  assert(sock->dfk);
-  if (STATE(sock) & TCP_SOCKET_LISTENING) {
-    ((dfk_tcp_socket_listen_obj_t*) sock->_arg.data)->close_yieldback =
-      DFK_THIS_CORO(sock->dfk);
-  } else {
-    sock->_arg.data = DFK_THIS_CORO(sock->dfk);
-  }
-  sock->_flags |= TCP_SOCKET_CLOSING;
-  DFK_INFO(sock->dfk, "{%p} start close", (void*) sock);
-  uv_close((uv_handle_t*) &sock->_socket, dfk__tcp_socket_on_close);
-  DFK_IO(sock->dfk);
-  DFK_INFO(sock->dfk, "{%p} returned from on_close callback", (void*) sock);
-  assert(STATE(sock) & TCP_SOCKET_CLOSING);
-  TO_STATE(sock, TCP_SOCKET_CLOSED);
-  return dfk_err_ok;
-}
-
-
 typedef struct dfk__tcp_socket_shutdown_data {
   dfk_coro_t* coro;
   dfk_tcp_socket_t* sock;
@@ -421,7 +309,7 @@ static void dfk__tcp_socket_wait_disconnect_callback(uv_poll_t* poll, int status
     DFK_DBG(wddata->coro->dfk, "{%p} client disconnected, resume", (void*) wddata->sock);
     DFK_IO_RESUME(wddata->coro->dfk, wddata->coro);
   } else if (events & (UV_READABLE | UV_WRITABLE)) {
-    /* do nothing thus far */
+     * do nothing thus far
   }
 }
 
@@ -518,7 +406,7 @@ static void dfk__tcp_socket_on_read(uv_stream_t* p, ssize_t nread, const uv_buf_
   assert(arg);
   DFK_INFO(sock->dfk, "(%p) %ld bytes read", (void*) sock, (long) nread);
   if (nread == 0) {
-    /* read returned EAGAIN or EWOULDBLOCK */
+    * read returned EAGAIN or EWOULDBLOCK
     return;
   } else if (nread < 0) {
     arg->err = dfk_err_sys;
@@ -557,66 +445,6 @@ static void dfk__tcp_socket_on_alloc(uv_handle_t* p, size_t hint, uv_buf_t* buf)
 }
 
 
-ssize_t dfk_tcp_socket_read(
-    dfk_tcp_socket_t* sock,
-    char* buf,
-    size_t nbytes)
-{
-  if (!sock || !buf) {
-    return dfk_err_badarg;
-  }
-
-  if (nbytes == 0) {
-    return 0;
-  }
-
-  if (STATE(sock) & TCP_SOCKET_READING) {
-    return dfk_err_inprog;
-  }
-
-  if (!(STATE(sock) & TCP_SOCKET_CONNECTED)) {
-    return dfk_err_badarg;
-  }
-
-  {
-    dfk_tcp_socket_read_async_arg_t arg;
-
-    DFK_DBG(sock->dfk, "{%p} read upto %lu bytes to %p",
-        (void*) sock, (unsigned long) nbytes, (void*) buf);
-    sock->_flags |= TCP_SOCKET_READING;
-
-    assert(sock->_arg.data == NULL);
-    arg.yieldback = DFK_THIS_CORO(sock->dfk);
-    arg.nbytes = nbytes;
-    arg.buf = buf;
-    arg.err = dfk_err_ok;
-    sock->_arg.data = &arg;
-
-    DFK_SYSCALL(sock->dfk, uv_read_start(
-        (uv_stream_t*) &sock->_socket,
-        dfk__tcp_socket_on_alloc,
-        dfk__tcp_socket_on_read));
-
-    DFK_IO(sock->dfk);
-
-    sock->_arg.data = NULL;
-    assert(STATE(sock) & TCP_SOCKET_READING);
-    sock->_flags ^= TCP_SOCKET_READING;
-
-    DFK_INFO(sock->dfk, "(%p) read returned %s, bytes read %lu",
-        (void*) sock, dfk_strerr(sock->dfk, arg.err),
-        (unsigned long) arg.nbytes);
-
-    if (arg.err != dfk_err_ok) {
-      sock->dfk->dfk_errno = arg.err;
-      return -1;
-    }
-
-    return arg.nbytes;
-  }
-}
-
-
 ssize_t dfk_tcp_socket_readv(
     dfk_tcp_socket_t* sock,
     dfk_iovec_t* iov,
@@ -628,7 +456,7 @@ ssize_t dfk_tcp_socket_readv(
   if (!niov) {
     return dfk_err_ok;
   }
-  /* optimizations are not supported yet */
+  * optimizations are not supported yet
   return dfk_tcp_socket_read(sock, iov[0].data, iov[0].size);
 }
 
@@ -742,3 +570,4 @@ ssize_t dfk_tcp_socket_writev(
   }
 }
 
+*/
